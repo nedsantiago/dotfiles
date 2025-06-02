eb442710ca12c9d6737863513989b05bf0da5a5d
Ned Santiago <nedsantiago@tutanota.com>
2025-06-02 20:08:01 +0800

    Added a texlive module for document preparation

---
+++ etc/nixos/modules/texlive.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+let
+    # Create a variable to hold the preferred packages
+    tex = (pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-full
+            # dvisvgm dvipng # for preview and export as html
+            # wrapfig amsmath ulem hyperref capt-of;
+            # enumitem substr xstring xifthen lastpage biblatex
+            # ifmtarg csquotes tabular
+            europasscv;
+    });
+in
+{
+    config = {
+        environment.systemPackages = with pkgs; [
+            tex
+        ];
+    };
+}

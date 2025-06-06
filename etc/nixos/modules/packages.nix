e77a0826d395a33985a6b98af92e2cc216ed875a
Ned Santiago <nedsantiago@tutanota.com>
2025-06-06 15:19:24 +0800

    Changed the tab alignment from four spaces to two spaces

--- etc/nixos/modules/packages.nix
+++ etc/nixos/modules/packages.nix
@@ -3,60 +3,60 @@
 { config, pkgs, ... }:
 
 {
-    # nixpkgs.config.allowUnfreePredicate = pkgs:
-    #     builtins.elem (lib.getName pkgs) [
-    #         "bws"
-    #     ];
+  # nixpkgs.config.allowUnfreePredicate = pkgs:
+  #     builtins.elem (lib.getName pkgs) [
+  #         "bws"
+  #     ];
 
-    config = {
+  config = {
 
-        # List packages installed in system profile. To search, run:
-        # $ nix search wget
-        environment.systemPackages = with pkgs; [
-            # Terminal Packages
-            alacritty       # primary terminal
-            kitty           # backup terminal
-            neovim          # text editor
-            tmux            # terminal panes
-            # mosh            # ssh with unstable internet
-            # zsh             # alternative shell
-            
-            # File Management Packages
-            bat             # print and view file to terminal
-            eza             # a robust file listing tool
-            fselect         # sql-like file management
-            dua             # disk usage
-            ripgrep         # find keywords in a file or directory
-            tree            # show file tree
-            exiftool        # read and edit file metadata
-            ranger          # a file explorer with file preview
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      # Terminal Packages
+      alacritty       # primary terminal
+      kitty           # backup terminal
+      neovim          # text editor
+      tmux            # terminal panes
+      # mosh            # ssh with unstable internet
+      # zsh             # alternative shell
+      
+      # File Management Packages
+      bat             # print and view file to terminal
+      eza             # a robust file listing tool
+      fselect         # sql-like file management
+      dua             # disk usage
+      ripgrep         # find keywords in a file or directory
+      tree            # show file tree
+      exiftool        # read and edit file metadata
+      ranger          # a file explorer with file preview
 
-            # Secrets Management Tools
-            pinentry-curses # pinentry for gpg
-            # sops            # file encryption and secrets manager
-            # pass            # password manager
-            # bws             # bitwarden secrets management
-            # jq              # json query interpreter (needed for bitwarden)
+      # Secrets Management Tools
+      pinentry-curses # pinentry for gpg
+      # sops            # file encryption and secrets manager
+      # pass            # password manager
+      # bws             # bitwarden secrets management
+      # jq              # json query interpreter (needed for bitwarden)
 
-            # My Development packages
-            git             # version control
-            # ollama          # large language model
+      # My Development packages
+      git             # version control
+      # ollama          # large language model
 
-            # Window Manager packages
-            neofetch        # summarizes system configuration
-            polybar         # system status bar
-            rofi            # startup application
-            dunst           # notification manager
-            feh             # wallpaper manager
-            pywal           # wallpaper manager and color selector
+      # Window Manager packages
+      neofetch        # summarizes system configuration
+      polybar         # system status bar
+      rofi            # startup application
+      dunst           # notification manager
+      feh             # wallpaper manager
+      pywal           # wallpaper manager and color selector
 
-            # My Packages
-            # flatpak         # package manager
-            firefox         # main browser
-            lynx            # text-based browser
-            wget            # cli downloader
-            unzip           # unzips downloaded files
-            xclip           # clipboard manager
-        ];
-    };
+      # My Packages
+      # flatpak         # package manager
+      firefox         # main browser
+      lynx            # text-based browser
+      wget            # cli downloader
+      unzip           # unzips downloaded files
+      xclip           # clipboard manager
+    ];
+  };
 }

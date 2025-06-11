47b28e55f9c3beccc933029b2167b924cec014c6
Ned Santiago <nedsantiago@tutanota.com>
2025-06-11 13:46:02 +0800

    Changed tab from 4 spaces to 2 spaces

--- etc/nixos/modules/app_defaults.nix
+++ etc/nixos/modules/app_defaults.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ... }:
 
 {
-    # Assign default apps
-    environment.sessionVariables = {
-        # WLR_NO_HARDWARE_CURSORS = "1";
-        # NIXOS_OZONE_WL = "1";
-        EDITOR = "neovim";
-        BROWSER = "firefox";
-        TERMINAL = "kitty";
-    };
+  # Assign default apps
+  environment.sessionVariables = {
+    # WLR_NO_HARDWARE_CURSORS = "1";
+    # NIXOS_OZONE_WL = "1";
+    EDITOR = "neovim";
+    BROWSER = "firefox";
+    TERMINAL = "kitty";
+  };
 }

246c28f0e698ad9e85e55030232429006be78618
Ned Santiago <nedsantiago@tutanota.com>
2025-06-12 22:03:48 +0800

    Changed tab from 4 spaces to 2 spaces

--- etc/nixos/modules/brightness.nix
+++ etc/nixos/modules/brightness.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ...}:
 
 {
-    config = {
-        environment.systemPackages = with pkgs; [
-            brightnessctl   # tool for changing screen brightness
-        ];
-    # Try changing the brightness using this terminal command:
-    # brightnessctl set 20%
-    };
+  config = {
+    environment.systemPackages = with pkgs; [
+      brightnessctl   # tool for changing screen brightness
+    ];
+  # Try changing the brightness using this terminal command:
+  # brightnessctl set 20%
+  };
 }

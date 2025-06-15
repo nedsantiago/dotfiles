3b9af4224eece516a2fac6706f241e5193b9f1d2
Ned Santiago <nedsantiago@tutanota.com>
2025-06-15 16:10:22 +0800

    Changed tab from 4 spaces to 2 spaces

--- etc/nixos/modules/docker.nix
+++ etc/nixos/modules/docker.nix
@@ -1,21 +1,21 @@
 { config, pkgs, ... }:
 
 {
-    config = {
-        environment.systemPackages = with pkgs; [
-            docker              # containerization
-            docker-compose      # container management
-        ];
-    };
+  config = {
+    environment.systemPackages = with pkgs; [
+      docker              # containerization
+      docker-compose      # container management
+    ];
+  };
 
-    config = {
-        virtualisation.docker = {
-            enable = true;
-            # For avoiding using sudo for each docker run
-            rootless = {
-                enable = true;
-                setSocketVariable = true;
-            };
-        };
+  config = {
+    virtualisation.docker = {
+      enable = true;
+      # For avoiding using sudo for each docker run
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
     };
+  };
 }

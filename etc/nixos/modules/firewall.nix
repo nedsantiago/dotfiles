8a5fadec6f4ad99de7c800dfebd79002c3515400
Ned Santiago <nedsantiago@tutanota.com>
2025-06-16 08:49:28 +0800

    Changed tab from 4 spaces to 2 spaces

--- etc/nixos/modules/firewall.nix
+++ etc/nixos/modules/firewall.nix
@@ -1,14 +1,14 @@
 { config, pkgs, ... }:
 
 {
-    # List services that you want to enable:
+  # List services that you want to enable:
 
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
 
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 }

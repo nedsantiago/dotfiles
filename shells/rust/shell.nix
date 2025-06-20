300948b67fe74d7de66d77a9d1f43f412765115d
Ned Santiago <nedsantiago@tutanota.com>
2025-06-20 08:30:02 +0800

    Updated the Nix Package repository link

--- shells/rust/shell.nix
+++ shells/rust/shell.nix
@@ -1,6 +1,9 @@
 let
   # Pinned nixpkgs, deterministic. Last updated: 2/12/21.
-  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/a58a0b5098f0c2a389ee70eb69422a052982d990.tar.gz")) {};
+  # pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/a58a0b5098f0c2a389ee70eb69422a052982d990.tar.gz")) {};
+
+  # Last updated: 2025-06-19 from https://github.com/NixOS/nixpkgs/commit/36ab78dab7da2e4e27911007033713bab534187b
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/36ab78dab7da2e4e27911007033713bab534187b.tar.gz")) {};
 
   # Rolling updates, not deterministic.
   # pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};

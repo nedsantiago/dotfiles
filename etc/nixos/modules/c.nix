c59e08bc847124a8f337ee5026c4f74c1839a7e8
Ned Santiago <nedsantiago@tutanota.com>
2025-08-08 20:00:38 +0800

    Added C language dev tools

--- etc/nixos/modules/c.nix
+++ etc/nixos/modules/c.nix
@@ -3,7 +3,9 @@
 {
   config = {
     environment.systemPackages = with pkgs; [
-      gcc    # C language compiler
+      gcc       # C language compiler
+      gnumake   # Control generation of non-source files from sources
+      valgrind  # C debugger
     ];
   };
 }

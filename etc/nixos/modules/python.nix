2f478af3d57d41d8e80256e5ef2642c3d77b66f4
Ned Santiago <nedsantiago@tutanota.com>
2025-08-27 19:42:11 +0800

    Added python jupyterlab and other modules

--- etc/nixos/modules/python.nix
+++ etc/nixos/modules/python.nix
@@ -3,8 +3,11 @@
 {
   config = {
     environment.systemPackages = with pkgs; [
-      (pkgs.python3.withPackages (python-pkgs: [
+      (pkgs.python312.withPackages (python-pkgs: [
         python-pkgs.pandas
+        python-pkgs.numpy
+        python-pkgs.matplotlib
+        python-pkgs.jupyterlab
       ]))
     ];
   };
